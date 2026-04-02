bdb4b0cb74db820494c3cf9c225a3236af620f86
aravindkummara1500-ux <aravindkummara1500@gmail.com>
2026-04-02 20:17:31 +0530

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name       = "dev-vpc"
+  cidr_block = "10.0.0.0/16"
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  azs = ["eu-west-1a", "eu-west-1b"]
+}
