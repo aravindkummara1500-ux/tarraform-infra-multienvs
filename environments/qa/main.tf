cd6f9ab07fbbdc0cb710bacaf3f21532e5fa05d6
aravindkummara1500-ux <aravindkummara1500@gmail.com>
2026-04-02 20:18:12 +0530

    Create main.tf

---
+++ environments/qa/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name       = "qa-vpc"
+  cidr_block = "10.1.0.0/16"
+
+  public_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets = ["10.1.3.0/24", "10.1.4.0/24"]
+
+  azs = ["eu-west-1a", "eu-west-1b"]
+}
